5c7519e213964d007f3c7a5629d0cd0221b60599
Sofistication <sordonez93@gmail.com>
2017-03-28 14:34:17 -0400

    Add authentication checks
    - `/datasets` and `/datasets/:id` now redirect to sign-in for unauthed
    - injected auth and flashmessages services into those routes
    - error telling user to sign in is displayed after user is redirected

--- app/routes/dataset.js
+++ app/routes/dataset.js
@@ -1,7 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  auth: Ember.inject.service(),
+  isAuthenticated: Ember.computed.alias('auth.isAuthenticated'),
+  flashMessages: Ember.inject.service(),
+
   model (params) {
-    return this.get('store').findRecord('dataset', params.dataset_id);
-  }
+    if (this.get('isAuthenticated')) {
+      return this.get('store').findRecord('dataset', params.dataset_id);
+    } else {
+      this.transitionTo('sign-in');
+      this.get('flashMessages')
+      .danger('Please sign in to view this page.');
+    }
+  },
 });
