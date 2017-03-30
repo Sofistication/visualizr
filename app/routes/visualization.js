587d45e70abc6f880039fa890afbf7d1140f50e4
Sofistication <sordonez93@gmail.com>
2017-03-30 12:09:15 -0400

    Load all datasets before loading visualization
    - changed model hook for `routes/visualization` to load all datasets
    - moved loading visualizations into a `.then()`

--- app/routes/visualization.js
+++ app/routes/visualization.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model (params) {
-    return this.get('store').findRecord('visualization', params.visualization_id);
+    return this.get('store').findAll('dataset')
+      .then(() => this.get('store').findRecord('visualization', params.visualization_id));
   }
 });
