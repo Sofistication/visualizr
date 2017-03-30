8b5f024cb23ed3397ac89398db4e0426fb3dbd84
Sofistication <sordonez93@gmail.com>
2017-03-30 10:31:55 -0400

    Successful create action
    - passed dataset model to creatVisualization action
    - created new Visualization record with color passed to action
    - set dataset of visualization to the one passed in
    - saved visualization on server
    - tested in browser

--- app/routes/dataset/save.js
+++ app/routes/dataset/save.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    createVisualization (color, model) {
+      let visualization = this.get('store').createRecord('visualization', {
+        color: color
+      });
+      visualization.set('dataset', model);
+      visualization.save()
+        .then(() => this.transitionTo('visualizations'));
+    }
+  }
 });
