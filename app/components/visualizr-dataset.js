0d5bd7890ea675ba1caec38b48ba8bdb5080b3a3
Sofistication <sordonez93@gmail.com>
2017-03-30 18:17:45 -0400

    Refactor creating visualization to use component
    - changed `visualizr-dataset` component to be save visualization
    - added action to component to create new visualization
    - refactored action on save route to handle action being sent
    
    TODO: refactor visualization/edit to use new component

--- app/components/visualizr-dataset.js
+++ app/components/visualizr-dataset.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  newViz: {
+    color: ''
+  },
+
+  actions: {
+    save (model) {
+      let data = this.get('newViz');
+      console.log(model);
+      this.sendAction('save', data, model);
+      this.set('newViz.color', '');
+    }
+  }
 });
