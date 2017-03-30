97a433e61d7cd81c2c30923b47f33ed257895ef9
Sofistication <sordonez93@gmail.com>
2017-03-30 18:44:43 -0400

    Protect change-password route from unatuhed users

--- app/routes/change-password.js
+++ app/routes/change-password.js
@@ -2,7 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   auth: Ember.inject.service(),
+  isAuthenticated: Ember.computed.alias('auth.isAuthenticated'),
   flashMessages: Ember.inject.service(),
+
+  model () {
+    if (!this.get('isAuthenticated')) {
+      this.transitionTo('sign-in');
+      this.get('flashMessages')
+        .danger('Please sign in!');
+    }
+  },
 
   actions: {
     changePassword (passwords) {
